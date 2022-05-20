dc68e1f9c386087300545aaa5a499d393477d798
Jose <joavelar@ucsc.edu>
2022-05-20 10:14:47 -0700

    Cutscene transitions

--- src/scene/Cutscene3.js
+++ src/scene/Cutscene3.js
@@ -8,5 +8,7 @@ class CS3 extends Phaser.Scene {
 
     create() {
         this.BG = this.tileSprite(0, 0, 640, 480, 'goodJob').setOrigin(0, 0);
+
+        this.input.on("pointerdown", () => this.scene.start("victoryScene"));
     }
 }
