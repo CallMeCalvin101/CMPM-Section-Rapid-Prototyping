d131be1aa695b1f919f30edcbd70c733bcaae846
Your Name <mjjasper@ucsc.edu>
2022-05-20 10:05:34 -0700

    Cutscene Addition

---
+++ src/scene/Cutscene2.js
@@ -0,0 +1,12 @@
+class CS2 extends Phaser.Scene {
+    constructor() {
+        super('cs2Scene');
+    }
+    preload() {
+        this.image.load('goodJob', './assets/Good-Job.png');
+    }
+
+    create() {
+        this.BG = this.tileSprite(0, 0, 640, 480, 'goodJob').setOrigin(0, 0);
+    }
+}
