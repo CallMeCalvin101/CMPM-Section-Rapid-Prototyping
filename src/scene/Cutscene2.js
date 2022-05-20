81581d62bc7aabb0b3921bf589dc1e7190d3d966
Your Name <mjjasper@ucsc.edu>
2022-05-20 10:23:57 -0700

    Completed Cutscene

--- src/scene/Cutscene2.js
+++ src/scene/Cutscene2.js
@@ -3,11 +3,11 @@ class Cutscene2 extends Phaser.Scene {
         super('cs2Scene');
     }
     preload() {
-        this.image.load('goodJob', './assets/Good-Job.png');
+        this.load.image('goodJob', './assets/Good-Job.png');
     }
 
     create() {
-        this.BG = this.tileSprite(0, 0, 640, 480, 'goodJob').setOrigin(0, 0);
+        this.BG = this.add.tileSprite(0, 0, 640, 480, 'goodJob').setOrigin(0, 0);
 
         this.input.on("pointerdown", () => this.scene.start("play3Scene"));
     }
