4f4c0f9a5ced5933845ad7b35a4a694b9a0f8941
JJHung999 <jjhung@ucsc.edu>
2022-05-20 10:31:47 -0700

    Resized and added the victory bg to victory.js

--- src/scene/victory.js
+++ src/scene/victory.js
@@ -2,9 +2,14 @@ class Victory extends Phaser.Scene {
     constructor() {
         super('victoryScene');
     }
+    preload() {
+        this.load.image('victoryBG', './assets/victoryscreen.png');
+        this.load.spritesheet('fireworks','./assets/victoryscreen_fireworks.png')
+    }
 
     create() {
         
+        this.victory = this.add.sprite(game.config.width/2, game.config.height/2, 'victoryBG')
         this.add.text(game.config.width / 2, game.config.height / 2, "VICTORY");
 
         // Go back to title screen on click.
